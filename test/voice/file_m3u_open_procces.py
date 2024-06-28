1a61416e70a7c8c6306578f678a8b95ac7192fc6
SeaFox <poluboyarova.m@gmail.com>
2024-06-28 15:22:32 +0300

    Continue

--- test/voice/file_m3u_open_procces.py
+++ test/voice/file_m3u_open_procces.py
@@ -2,8 +2,9 @@ import os.path
 import time
 import vlc
 
-playlist_m3u = open('list.m3u')
-# playlist_m3u = open('8941.m3u')
+# playlist_m3u = open('list.m3u')
+# playlist_m3u = open('pl_yt.m3u')
+playlist_m3u = open('8941.m3u')
 print('playlist_m3u:', playlist_m3u.__sizeof__())
 
 print(time.time())
@@ -19,13 +20,19 @@ list_for_tuple = list()
 for line in playlist_list:
     if line[0] == '#':
         continue
-    list_for_tuple.append(os.path.abspath(line[8:]))
+    elif line[0:5] == 'file:':
+        list_for_tuple.append(os.path.abspath(line[8:]))
+    elif line[0:6] == 'https:':
+        # list_for_tuple.append(os.path.abspath(line))
+        list_for_tuple.append(line.rstrip())
 
 playlist_in_tuple = tuple(list_for_tuple)
 print(time.time())
 
-print('list_for_tuple:', list_for_tuple.__sizeof__())
-print('playlist_in_tuple:', playlist_in_tuple.__sizeof__())
+print('list_for_tuple.__sizeof__:', list_for_tuple.__sizeof__())
+print('len list_for_tuple:', len(list_for_tuple))
+print('playlist_in_tuple.__sizeof__:', playlist_in_tuple.__sizeof__())
+print('len playlist_in_tuple:', len(playlist_in_tuple))
 
 player = vlc.Instance()
 
